513d38bf7e0abbbd367a08a4cd6ec560719a67e6
Armin Krauss <armin.krauss@utoronto.ca>
2014-03-12 14:50:26 -0400

    updating gitignore file

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 app.use(express.bodyParser()); // this is needed to parse the body of requests like POST and PUT
+// var authenticated = false;
 
 app.get('/test', function(req, res) {
-  res.statusCode = 404;
-  return res.send('access denied');
+  var googleToken = req.param('google_access_token');
+
+  if (googleToken === "armin") {
+    res.json({'hello':'world'})
+  } else {
+    res.statusCode = 404;
+    return res.send('access denied');
+  }
 });
 
 app.listen(process.env.PORT || 8000);
